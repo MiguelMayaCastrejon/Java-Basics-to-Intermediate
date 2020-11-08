a6303d5aff122bb8a1e01d701ddb45db0ca10321
Mike <mike.maya@ciencias.unam.mx>
2020-11-08 16:21:07 -0600

    A little example about constants.

---
+++ Introduction/Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+    public static void main(String[] args) {
+        //Variables
+        int number = 10;
+        number = 20;
+        System.out.println(number);
+
+        final int number2 = 2; //final is for constants.
+        //number2 =10; It cannot be reassigned because it is a constant.
+        System.out.println(number2);
+    }
+}
